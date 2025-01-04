cedc898e303852c8e035ed571486aee66f1d3cef
Kojok <kojokamo120@gmail.com>
2025-01-04 13:59:52 +0900

    実行ファイルからの相対パスに修正

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: kokamoto <kojokamo120@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 13:31:56 by kokamoto          #+#    #+#             */
-/*   Updated: 2025/01/04 12:18:01 by kokamoto         ###   ########.fr       */
+/*   Updated: 2025/01/04 13:48:11 by kokamoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,13 @@ void	init_game(t_game *game)
 	game->moves = 0;
 	game->collectibles = 0;
 	game->player_img = mlx_xpm_file_to_image(game->mlx,
-			"../images-xpm/player.xpm", &img_width, &img_height);
-	game->wall_img = mlx_xpm_file_to_image(game->mlx, "../images-xpm/wall.xpm",
+			"images-xpm/player.xpm", &img_width, &img_height);
+	game->wall_img = mlx_xpm_file_to_image(game->mlx, "images-xpm/wall.xpm",
 			&img_width, &img_height);
 	game->collectible_img = mlx_xpm_file_to_image(game->mlx,
-			"../images-xpm/collectible.xpm", &img_width, &img_height);
-	game->exit_img = mlx_xpm_file_to_image(game->mlx, "../images-xpm/exit.xpm",
+			"images-xpm/collectible.xpm", &img_width, &img_height);
+	game->exit_img = mlx_xpm_file_to_image(game->mlx, "images-xpm/exit.xpm",
 			&img_width, &img_height);
 	game->floor_img = mlx_xpm_file_to_image(game->mlx,
-			"../images-xpm/floor.xpm", &img_width, &img_height);
+			"images-xpm/floor.xpm", &img_width, &img_height);
 }
