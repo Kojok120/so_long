3897d7b5f79596f82f2c26a24531624dc22d5423
Koji Okamoto <kokamoto@c2r1s1.42tokyo.jp>
2025-01-04 12:20:00 +0900

    フォルダ構成変更

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: kokamoto <kojokamo120@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 13:31:56 by kokamoto          #+#    #+#             */
-/*   Updated: 2025/01/02 13:32:32 by kokamoto         ###   ########.fr       */
+/*   Updated: 2025/01/04 12:18:01 by kokamoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,17 @@ void	init_game(t_game *game)
 
 	game->mlx = mlx_init();
 	game->win = mlx_new_window(game->mlx, WINDOW_WIDTH, WINDOW_HEIGHT,
-			"so_long");
+			"so_long kokamoto");
 	game->moves = 0;
 	game->collectibles = 0;
-	game->player_img = mlx_xpm_file_to_image(game->mlx, "player.xpm",
+	game->player_img = mlx_xpm_file_to_image(game->mlx,
+			"../images-xpm/player.xpm", &img_width, &img_height);
+	game->wall_img = mlx_xpm_file_to_image(game->mlx, "../images-xpm/wall.xpm",
 			&img_width, &img_height);
-	game->wall_img = mlx_xpm_file_to_image(game->mlx, "wall.xpm", &img_width,
-			&img_height);
-	game->collectible_img = mlx_xpm_file_to_image(game->mlx, "collectible.xpm",
+	game->collectible_img = mlx_xpm_file_to_image(game->mlx,
+			"../images-xpm/collectible.xpm", &img_width, &img_height);
+	game->exit_img = mlx_xpm_file_to_image(game->mlx, "../images-xpm/exit.xpm",
 			&img_width, &img_height);
-	game->exit_img = mlx_xpm_file_to_image(game->mlx, "exit.xpm", &img_width,
-			&img_height);
-	game->floor_img = mlx_xpm_file_to_image(game->mlx, "floor.xpm", &img_width,
-			&img_height);
+	game->floor_img = mlx_xpm_file_to_image(game->mlx,
+			"../images-xpm/floor.xpm", &img_width, &img_height);
 }
